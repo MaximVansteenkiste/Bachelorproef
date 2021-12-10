033affb230bbb7289f1014959df4deafa28ba3e4
MaximVansteenkiste <vsmaxim@me.com>
2021-12-10 15:13:35 +0100

    New projects

---
+++ tooling/webpack/bar-react-main/src/components/Input.js
@@ -0,0 +1,28 @@
+const Input = ({
+  className = "",
+  icon,
+  register = () => {},
+  name = "",
+  ...props
+}) => {
+  return (
+    <div className="relative w-full h-8">
+      <input
+        {...props}
+        className={
+          "rounded-md bg-card-lighter px-2 w-full h-full focus:ring-2 focus:ring-accent focus:border-transparent outline-none " +
+          className
+        }
+        name={name}
+        {...register(name)}
+      ></input>
+      {icon && (
+        <div className="absolute right-2 top-0 h-full grid place-items-center">
+          {icon}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Input;
